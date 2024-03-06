0f32b3bbc229400993a9e0123eeb61b81841b1bc
Daniel Santiago Torres <RulosS29@outlook.es>
2024-03-05 20:09:30 -0500

    C

--- Spring/src/main/java/net/codejava/SpringBootHelloWorld/SpringBootHelloWorldApplication.java
+++ Spring/src/main/java/net/codejava/SpringBootHelloWorld/SpringBootHelloWorldApplication.java
@@ -2,6 +2,7 @@ package net.codejava.SpringBootHelloWorld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,14 +13,15 @@ public class SpringBootHelloWorldApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootHelloWorldApplication.class, args);
 	}
+	@CrossOrigin("http://localhost:3000")
 	@RequestMapping("/")
-	public String home(){
-		return "<h1>Hello world Spring Boot</h1>";
+	public String home() {
+		return "Hello world Spring Boot";
 	}
-
+	@CrossOrigin("http://localhost:3000")
 	@RequestMapping("/new")
 	public String nuevo(){
-		return "<h1>Hello World</h1>";
+		return "Hello World";
 	}
 
 

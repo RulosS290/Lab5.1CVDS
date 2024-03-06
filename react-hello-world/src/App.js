fb1253d1047f02af3da21fa63d4f040855657072
Diegoc04 <diego.fercho.castellanos@gmail.com>
2024-03-06 13:07:02 -0500

    cambios

--- react-hello-world/src/App.js
+++ react-hello-world/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Daniel Torres
+          Daniel Torres-Diego Castellanos
         </p>
         <HelloWorld/>
       </header>
